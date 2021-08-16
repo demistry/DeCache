ef6ed81d743b089df84e2d55dc408f16db5e2e78
David Ilenwabor <dave11ilen@gmail.com>
2021-08-16 03:33:54 +0100

    Initial Commit

---
+++ DeCache/DeCache.h
@@ -0,0 +1,18 @@
+//
+//  DeCache.h
+//  DeCache
+//
+//  Created by David Ilenwabor on 16/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeCache.
+FOUNDATION_EXPORT double DeCacheVersionNumber;
+
+//! Project version string for DeCache.
+FOUNDATION_EXPORT const unsigned char DeCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeCache/PublicHeader.h>
+
+
